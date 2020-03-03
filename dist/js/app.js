ae5d083f3893316e7e5ae370b29ca58c905abac9
Emre Kayikcilar <emreebrukayikcilar@gmail.com>
2020-03-03 16:03:54 +0300

    Books, Customers, Edit-Book, New-Book pages added

--- dist/js/app.js
+++ dist/js/app.js
@@ -10600,4 +10600,13 @@ return jQuery;
 
 },{}],2:[function(require,module,exports){
 window.$ = window.jQuery = require('jquery');
+
+// Bulma's custom file input is not working to show the file name that is chosen by the user
+// It should implemented by using javascript
+const fileContainer = document.querySelector('.file-name');	// File name container
+const fileInput = document.getElementById('coverimage');	// Native file input
+fileInput.addEventListener('change', function(event){
+	const filename = fileInput.files[0].name;
+	fileContainer.innerHTML = filename;
+});
 },{"jquery":1}]},{},[2])
--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,10 @@
 window.$ = window.jQuery = require('jquery');
+
+// Bulma's custom file input is not working to show the file name that is chosen by the user
+// It should implemented by using javascript
+const fileContainer = document.querySelector('.file-name');	// File name container
+const fileInput = document.getElementById('coverimage');	// Native file input
+fileInput.addEventListener('change', function(event){
+	const filename = fileInput.files[0].name;
+	fileContainer.innerHTML = filename;
+});
